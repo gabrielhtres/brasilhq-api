eb918381bd736179d663bb438b1f2643f6b663c4
Gabriel <gabriel.tres@beatscode.com>
2023-01-09 17:05:02 -0300

    Criada transformação da senha cadastrada em hash no bd

--- src/Controllers/Usuario.js
+++ src/Controllers/Usuario.js
@@ -1,14 +1,22 @@
-const usuarioModel = require("../Models/Usuario");
+const bcrypt = require("bcrypt");
 
+const usuarioModel = require("../Models/Usuario");
 const auth = require("../Middlewares/auth");
 
 const fazerLogin = async (req, res) => {
   const { usuario, senha } = req.body;
 
-  const retorno = await usuarioModel.findOne({ usuario, senha });
+  const usuarioRecuperado = await usuarioModel.findOne({ usuario });
+  console.log(usuarioRecuperado);
 
-  res.status(200).send(auth.gerarToken(retorno._id));
+  const loginValido = await bcrypt.compare(senha, usuarioRecuperado.senha);
+
+  if (loginValido) {
+    res.status(200).send(auth.gerarToken(usuarioRecuperado._id));
+  }
 };
+
+// const retorno = await usuarioModel.findOne({ usuario, senha });
 
 const recuperarTodos = (req, res) => {
   usuarioModel.find({}, (err, result) => {
@@ -19,9 +27,18 @@ const recuperarTodos = (req, res) => {
 
 const registrarUsuario = (req, res) => {
   const { nome, usuario, senha } = req.body;
-  usuarioModel.create({ nome, usuario, senha }, (err, result) => {
-    res.status(201);
-    res.send(result);
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) console.log(err);
+
+    bcrypt.hash(senha, salt, (err, hash) => {
+      if (err) console.log(err);
+
+      usuarioModel.create({ nome, usuario, senha: hash }, (err, result) => {
+        res.status(201);
+        res.send(result);
+      });
+    });
   });
 };
 
